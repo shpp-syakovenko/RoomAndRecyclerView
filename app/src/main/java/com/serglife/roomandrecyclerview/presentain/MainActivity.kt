78925ff48674db8a1ec784feedff2f734d508dd9
Sergey <serglife777@gmail.com>
2021-11-20 01:07:13 +0200

    Initial commit

---
+++ app/src/main/java/com/serglife/roomandrecyclerview/presentain/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.serglife.roomandrecyclerview.presentain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.serglife.roomandrecyclerview.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
